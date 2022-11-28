7dd2462dfb7c7818eb0876e083569bba30f54cca
Tangani <tanganimoyo@gmail.com>
2022-11-29 00:50:47 +0200

    Modularising the use of crates

--- src/scripts/read_external_files.rs
+++ src/scripts/read_external_files.rs
@@ -1,22 +1,24 @@
-// use super::read_external_files::function;
-
 use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-pub fn read_file() -> Result<(), Box<dyn Error>> {
+// pub fn read_file(file_location: &String) -> Result<(), Box<dyn Error>> {
 
-    let _path = Path::new("/Users/molisa/Documents/rust/projects/src/test.txt");
+pub fn read_file(file_location: &str) -> Result<Vec<String>, Box<dyn Error>> {
+
+    let _path = Path::new(file_location);
     let _file: Result<String, std::io::Error> = fs::read_to_string(&_path);
 
     println!("{:?}", _file);
 
-    let binding: String = file?;
-    let sentences = binding.split("\n");
+    let binding: String = _file?;
+    let _sentences: Vec<String> = binding.split("\n").map(|s| s.to_string()).collect();
 
+    /*
     for line in sentences {
         println!("{:?}", line);
     }
-    Ok(())
-
+     */
+    Ok(_sentences)
+    // return sentences;
 }
